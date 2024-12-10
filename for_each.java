7a9a27f93a0a874cdce8ecfdaf9e30d7d82c3188
Hasini E <hasini1530@gmail.com>
2024-12-10 16:06:10 +0530

    for_each.java

---
+++ for_each.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class for_each {
+  public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     String a[]={"apple","orange","mango"};
+     for(String b:a)
+     System.out.print(b+",");
+     
+  }
+}
