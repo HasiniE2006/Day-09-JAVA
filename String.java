33e8db0288f9be4b2c13b131756d1ae86bc5896d
Hasini E <hasini1530@gmail.com>
2024-12-10 12:24:28 +0530

    String.java

---
+++ String.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+public static void main(String[] args) {
+    String a ="Hey";
+    String b = new String("Hai");
+    System.out.println(a);
+    System.out.println(b);
+}
+}
